8242ec6ce562f39687292a3464fa088c01c47fee
AEPAC CLIENT <AEPAC CLIENT@DESKTOP-8H2JL49>
2022-12-06 10:36:44 +0530

    Commiting test 2(as per changes)

--- src/Demo/Sample2.java
+++ src/Demo/Sample2.java
@@ -3,6 +3,6 @@ package Demo;
 public class Sample2 
 {
 			public static void main(String[] args) {
-				System.out.println("Git_project changes made");
+				System.out.println("As per task given lead i have completed");
 			}
 }
